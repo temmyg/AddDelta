cdd79ad9f1506e6061cf826ac143ee5ad02edbd6
Ge Zhou (HCL America Inc) <v-zhouge@microsoft.com>
2020-06-20 18:11:04 -0700

    1st version

---
+++ AddDelta/index.js
@@ -0,0 +1,16 @@
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+    
+    if (req.query.ground || (req.body && req.body.ground)) {
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: 10 + +(req.query.ground || req.body.ground)
+        };
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: "Please pass a ground value on the query string or in the request body"
+        };
+    }
+};
